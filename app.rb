23ed985aaee764458c44e05d96287903a65305c4
adam-larosa <adam.larosa@flatironschool.com>
2020-12-10 14:54:19 -0800

    Refactor

--- app.rb
+++ app.rb
@@ -1,22 +1,10 @@
 require_relative 'config/environment'
 class App < Sinatra::Base
-	get '/reversename/:name' do
-		params[:name].reverse
-	end
-
-	get '/square/:number' do
-		(params[:number].to_i * params[:number].to_i).to_s
-	end
-
-	get '/say/:number/:phrase' do
-		"#{params[:phrase]} " * params[:number].to_i
-	end
-
-	get '/say/:word1/:word2/:word3/:word4/:word5' do
-		params.values.join(" ") + "."
-	end
-
-	get '/:operation/:number1/:number2' do
+	get('/reversename/:name') {params[:name].reverse}
+	get('/square/:number') {(params[:number].to_i * params[:number].to_i).to_s}
+	get('/say/:number/:phrase') {"#{params[:phrase]} " * params[:number].to_i}
+	get('/say/:word1/:word2/:word3/:word4/:word5') {params.values.join(" ") + "."}
+	get('/:operation/:number1/:number2') {
 		case params[:operation]
 		when "add"
 			(params[:number1].to_i + params[:number2].to_i).to_s
@@ -27,5 +15,5 @@ class App < Sinatra::Base
 		when "divide"
 			(params[:number1].to_i / params[:number2].to_i).to_s
 		end
-	end
+	}
 end
